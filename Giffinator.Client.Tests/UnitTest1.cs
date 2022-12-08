067319c323a13753693f1ecf8486ad6fbeafcf4f
Scott Galloway <503770+scottgal@users.noreply.github.com>
2022-12-08 13:07:08 +0000

    Initial

---
+++ Giffinator.Client.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace Giffinator.Client.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ Giffinator.Client/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Giffinator.Client;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
